368336cd92a07fd53c943ac2d75bcb991821cc6e
Alex <alexanderjshopov@gmail.com>
2017-11-08 23:41:00 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("user flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("user flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match!");
+	} else {
+		alert("try again");
+	}
+}
