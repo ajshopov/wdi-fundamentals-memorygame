52f3bcd49904e88979feac0eaefa0605115019ec
Alex <alexanderjshopov@gmail.com>
2017-11-21 14:19:16 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -18,12 +18,13 @@ var cards = [
 	{
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/king-of-dimonds.png"
+	cardImage: "images/king-of-diamonds.png"
 	}
 ];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
+	
 	if (cardsInPlay.length === 2){
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("you found a match!");
@@ -33,13 +34,26 @@ var checkForMatch = function(){
 	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i=0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard)
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+};
+
+createBoard();
+
